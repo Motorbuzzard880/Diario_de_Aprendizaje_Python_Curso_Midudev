7dada636c539c7dc91c9c1027800b19658e37f99
motorbuzzard <l.navarro2022@icloud.com>
2025-05-21 19:58:17 +0200

    daily 21-05

--- Ejercicios/ejercicios_funciones_sueltos.py
+++ Ejercicios/ejercicios_funciones_sueltos.py
@@ -42,3 +42,25 @@ if es_palindromo(palabra):
     print(f"{palabra} es un palíndromo. ¡Correcto!")
 else:
     print(f"{palabra}, no es un palindromo")
+
+
+
+# Ejercicio_04 Calcular el área de un triángulo
+# Escribe una función llamada area_triangulo que reciba la base
+# y altura de un triángulo y devuelva su área.
+
+def area_triangulo(base, altura):
+    """
+    Calcula el área de un triángulo dado su base y altura.
+    Args:
+        base (int/float): Base del triángulo. Debe ser un valor positivo.
+        altura (int/float): Altura del triángulo. Debe ser un valor positivo.
+    Returns:
+        float: Área del triángulo (base * altura / 2). 
+        Retorna 0.0 si la base o altura son <= 0 (inválidos)."""
+
+    if base <= 0 or altura <=0:
+        return 0.0
+    return float(base * altura / 2.0)
+
+print(f"El aera del triangulo es: {area_triangulo(5,-4)}")
