280bacc6e2696c4bf82c63d2d80940f1bd54f21c
motorbuzzard <l.navarro2022@icloud.com>
2025-05-28 19:58:24 +0200

    28-5

---
+++ Ejercicios/Regex.py
@@ -0,0 +1,32 @@
+﻿
+
+from os import system
+if system("clear") != 0: system("cls")
+
+import re
+
+
+
+texto = """En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda.
+El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la podadera.
+Frisaba la edad de nuestro hidalgo con los cincuenta años; era de complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de la caza. Quieren decir que tenía el sobrenombre de Quijada, o Quesada, que en esto hay alguna diferencia en los autores que deste caso escriben; aunque por conjeturas verosímiles se deja entender que se llamaba Quejana. Pero esto importa poco a nuestro cuento; basta que en la narración dél no se salga un punto de la verdad.
+Es, pues, de saber que este sobredicho hidalgo, los ratos que estaba ocioso (que eran los más del año), se daba a leer libros de caballerías con tanta afición y gusto, que olvidó casi de todo punto el ejercicio de la caza, y aun la administración de su hacienda; y llegó a tanto su curiosidad y desatino en esto, que vendió muchas hanegas de tierra de sembradura para comprar libros de caballerías en que leer, y así, llevó a su casa todos cuantos pudo haber dellos.
+De tanto leer estos libros, se le secó el cerebro de modo que vino a perder el juicio. Llenósele la fantasía de todo aquello que leía en los libros, así de encantamentos como de pendencias, batallas, desafíos, heridas, requiebros, amores, tormentas y disparates imposibles; y asentósele de tal modo en la imaginación que era verdad toda aquella máquina de aquellas soñadas invenciones que leía, que para él no había otra historia más cierta en el mundo."""
+
+pattern = "hidalgo"
+
+resultado = re.search(pattern, texto)
+
+if resultado:
+    print(f"He encontrado la palabra: {resultado.group()}")
+else:
+    print("No he encontrado la palabra")
+
+ocurrencias = re.findall(pattern, texto)
+print(f"La palabra aparece {len(ocurrencias)} veces.")
+
+
+#Tambien podria hacer una busqueda que no distinga mayúsculas/minúsculas
+
+# resultado = re.search(pattern, texto, re.IGNORECASE) 
+
